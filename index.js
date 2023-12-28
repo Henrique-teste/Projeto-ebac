d5774a3f5b833efba31e93763e90ef8b0fd5b251
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2023-12-28 18:22:21 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 })
 $('form').on('submit',function(e){
     e.preventDefault();
+    $( 'ul li' ). clique em ( função () {
+    $( isto ). toggleClass ( 'marcado' );
+  });
     const clickTarefa = $('#nova-tarefa').val();
     const tarefaNova = $('<li></li>')
 $(`<div class="efeito-riscar">
