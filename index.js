5174c81306ef53047a50dbfa8720711b4fb746a2
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2023-12-28 23:12:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
 
 })
+$('ul li').click(function() {
+    $(this).toggleClass('checked');
+});
+
 $('form').on('submit',function(e){
     e.preventDefault();
     const clickTarefa = $('#nova-tarefa').val();
     const tarefaNova = $('<li></li>')
-$(`<div class="efeito-riscar">
-<label for="nova-tarefa">${clickTarefa}</label>
-</div>`).appendTo(tarefaNova);
+$(`
+<li>
+    <label>${clickTarefa}</label>
+</li>`).appendTo(tarefaNova);
 $(tarefaNova).appendTo('ul');
-$('#nova-tarefa').val('')
-$('ul li').click(function(){
-    $(isto).toggleClass('marcado');
-})
 
+
+$('ul li').click(function() {
+    $(this).toggleClass('checked');
+});
+$('#nova-tarefa').val('')
 
 })
